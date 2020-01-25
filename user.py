6f690edd5b2ad72a56fa1928b6fc08cf4c8669ab
habibahassan <halimaadan92@gmail.com>
2020-01-25 15:19:57 +0300

    added instance for if user exist on user class

--- user.py
+++ user.py
@@ -16,22 +16,22 @@ class User:
         self.first_name = first_name
         self.second_name = second_name
         self.password = password
-    # @classmethod
-    # def display_users(cls):
-    #     '''
-    #     method to display all the users
-    #     '''
-    #     return cls.user_list
-    #  @classmethod
-    # def user_exist(cls, password):
-    #     '''
-    #     method to check if a user and
-    #     their details exist
-    #     '''
-    #     for user in cls.user_list:
-    #         if user.password == password:
-    #             return True
-    #     return False
+    @classmethod
+    def display_users(cls):
+        '''
+        method to display all the users
+        '''
+        return cls.user_list
+     @classmethod
+    def user_exist(cls, password):
+        '''
+        method to check if a user and
+        their details exist
+        '''
+        for user in cls.user_list:
+            if user.password == password:
+                return True
+        return False
 
     # @classmethod
     # def find_account(cls, password2):
