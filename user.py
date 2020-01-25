2b4bb3371647db55c9aa80835b6457a97f7730f3
habibahassan <halimaadan92@gmail.com>
2020-01-25 15:21:47 +0300

    added instance for if finding account on user class

--- user.py
+++ user.py
@@ -33,11 +33,11 @@ class User:
                 return True
         return False
 
-    # @classmethod
-    # def find_account(cls, password2):
-    #     '''
-    #     method that finds an account by its name
-    #     '''
-    #     for user in cls.user_list:
-    #         if user.password == password2:
-    #             return user
+    @classmethod
+    def find_account(cls, password2):
+        '''
+        method that finds an account by its name
+        '''
+        for user in cls.user_list:
+            if user.password == password2:
+                return user
