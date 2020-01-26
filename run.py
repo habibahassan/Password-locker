5c47c83632d33e57aed347e860d54aef7d85d4a8
habibahassan <halimaadan92@gmail.com>
2020-01-26 21:04:02 +0300

    added if and else statement

--- run.py
+++ run.py
@@ -47,4 +47,120 @@ def save_users(user):
     function to save a user
     '''
     user.save_user()
+def intro():
+    print("Hey! Welcome to Password Locker")
+    print('\n')
+    print("Please sign up for an accout to enjoy services")
 
+    while True:
+        print("⇨ Use these short codes : su - Sign up, lg - login, du-display all users, ex-Exit app ")
+        print('-'*64)
+        print('\n')
+        short_code = input().lower()
+        print('\n')
+        if short_code == 'zu':
+            print("New User")
+            print("-"*9)
+
+            print("Enter you first name...")
+            f_name = input()
+
+            print("Enter your second name...")
+            s_name = input()
+
+            print("Enter your password...")
+            password = input()
+            print('\n')
+
+            save_users(create_user(f_name, s_name, password))
+            print('\n')
+            print(
+                f"⇨ Congratulations {f_name} {s_name}, you now have an account \n")
+            print('\n')
+        elif short_code == 'lg':
+
+            print("Enter the first name of your registered account")
+            account_name = input()
+            print('\n')
+
+            authentification = getpass.getpass('Password:')
+            if check_existing_user(authentification):
+                search_account = find_account(authentification)
+
+                while True:
+                    print(
+                        f"⇨ Welcome {search_account.first_name} {search_account.second_name} \n")
+                    print(
+                        "⇨ cc-To create new password, vc-To view all your passwords, ex-exit account \n ")
+                    print('-'*80)
+                    short_code = input().lower()
+                    if short_code == 'cc':
+                        print("New password")
+                        print('-'*14)
+                        print("Enter account name")
+                        account_name = input()
+                        print("Make a password \n")
+                        print(
+                            "To make your own password press- a, to generate a password press - g \n")
+                        print('-'*50)
+                        generate = input()
+                        print('\n')
+
+                        if generate == 'g':
+                            letters = string.ascii_letters + string.digits
+                            gpassword = ''.join(random.choice(letters)
+                                                for i in range(9))
+                            print(
+                                f"Your new generated password is: {gpassword} \n")
+                            passkey = gpassword
+
+                        elif generate == 'a':
+                            print("Enter its password")
+                            passkey = input()
+                            print('\n')
+                        print(f"{account_name} has been saved")
+
+                        save_password(create_password(
+                            account_name, passkey))
+
+                    elif short_code == 'vc':
+                        if display_passwords:
+                            print(
+                                "⇨ Here is a list of all your accounts and passwords \n")
+                            for password in display_passwords():
+                                print(
+                                    f"Account name: {password.account_name} - password: {password.passkey}")
+                    elif short_code == 'dc':
+                        print("Which password would you like to delete?")
+                        del_account = input()
+                        if del_account == account_name:
+
+                            password.password_list.remove(password)
+                            print("password deleted")
+                        else:
+                            print("No match of such a password")
+
+                    elif short_code == 'ex':
+                        print("You have exited your account \n")
+                        break
+            else:
+                print("The password was incorrect \n")
+                print('\n')
+        elif short_code == 'du':
+            print("Here is a list of all the users\n")
+            for user in display_users():
+                print(f"{user.first_name} {user.second_name} \n")
+
+        elif short_code == 'ex':
+
+            print("Ok well. See yah! \n")
+
+            break
+        else:
+            print("☹ _ I cant understand this, please use these codes \n")
+
+
+if __name__ == '__main__':
+
+    intro()
+    
